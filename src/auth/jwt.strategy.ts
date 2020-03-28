f6737c7c65abd8ef5e60b01283784ac2d72da244
Ricardo <ricardoarrobo@gmail.com>
2020-03-28 16:21:43 -0500

    feat(config): add configuration of the project

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -6,6 +6,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { User } from './user.entity';
 
+import * as config from 'config';
+const jwtConfig = config.get('jwt');
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor(
@@ -14,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: 'secret-seed',
+            secretOrKey: process.env.JWT_SECRET || jwtConfig.secret,
         });
     }
 
